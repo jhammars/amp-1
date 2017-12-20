58aee6cb3d4e25de784e438fde9b90c1ed9c7d7e
dolebas <johan.hammarstrm@gmail.com>
2017-12-20 17:38:06 +0100

    .

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,21 @@
 const functions = require('firebase-functions');
+var data = require('./../json/amp-list-urls.json');
+
+// let app = express();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+let myObj = {
+    first: 'first value',
+    second: 'second value',
+};
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send(data);
+});
+
+// Get data from HN API
+
+// Return data on request
