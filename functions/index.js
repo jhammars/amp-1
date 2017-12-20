2c68956f171c6023d5d07411a590e8ec9dc95792
dolebas <johan.hammarstrm@gmail.com>
2017-12-20 18:11:40 +0100

    .

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,11 @@
 const functions = require('firebase-functions');
-var data = require('./../json/amp-list-urls.json');
+var data = require('./json/amp-list-urls.json');
+var express = require('express');
+// var app = express();
+var cors = require('cors');
+
+// app.use(cors());
+// functions.use(cors());
 
 // let app = express();
 
@@ -7,15 +13,30 @@ var data = require('./../json/amp-list-urls.json');
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 
-let myObj = {
-    first: 'first value',
-    second: 'second value',
-};
+// let myObj = {
+//     first: 'first value',
+//     second: 'second value',
+// };
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send(data);
-});
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//     response.send(data);
+// });
 
 // Get data from HN API
 
 // Return data on request
+
+/* Express with CORS */
+const app2 = express();
+app2.use(cors({ origin: true }));
+app2.get('*', (request, response) => {
+    response.send(data);
+});
+
+// const helloWorld = functions.https.onRequest(app2);
+
+exports.helloWorld = functions.https.onRequest(app2);
+
+// module.exports = {
+//     helloWorld,
+// };
